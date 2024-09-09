40f475ebc806e2921684c33dbb1c2d67c6eb8266
ahmaddani227 <putraahmaddani833@gmail.com>
2024-09-09 22:35:40 +0700

    Next Pages - E7 (Api Routes)

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,16 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
 
   useEffect(() => {
@@ -11,9 +19,18 @@ const ProductPage = () => {
     }
   }, []);
 
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((response) => setProducts(response.data));
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Products</h1>
+      {products.map((product: productType, i) => (
+        <div key={i}>{product.name}</div>
+      ))}
     </div>
   );
 };
