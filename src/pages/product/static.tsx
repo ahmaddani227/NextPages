1410e20a4e30837e7621a44a59575395cfbd48e2
Ahmad Dani <putraahmaddani833@gmail.com>
2025-04-24 17:02:40 +0700

    Next Pages - EP10(SSG)

---
+++ src/pages/product/static.tsx
@@ -0,0 +1,25 @@
+import ProductView from "@/views/Product";
+import { ProductType } from "@/types/product.type";
+
+// default nya static site generation jika tidak fetch data
+const ProductPage = (props: { products: ProductType[] }) => {
+  const { products } = props;
+
+  return (
+    <div>
+      <ProductView products={products} />
+    </div>
+  );
+};
+
+export default ProductPage;
+
+export async function getStaticProps() {
+  // fetch data
+  const res = await fetch("http://localhost:3000/api/products");
+  const response = await res.json();
+
+  return {
+    props: { products: response.data },
+  };
+}
